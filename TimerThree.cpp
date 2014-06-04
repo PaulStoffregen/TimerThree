2ff681194cb366bc1114abbbe35d0a5d4f6b8c72
PaulStoffregen <paul@pjrc.com>
2014-06-04 16:11:20 -0700

    Port to Teensy 3.1

--- TimerThree.cpp
+++ TimerThree.cpp
@@ -5,6 +5,7 @@
  *  Modified June 2009 by Michael Polli and Jesse Tane to fix a bug in setPeriod() which caused the timer to stop
  *  Modified Oct 2009 by Dan Clemens to work with timer3 of the ATMega1280 or Arduino Mega
  *  Modified April 2012 by Paul Stoffregen
+ *  Modified again, June 2014 by Paul Stoffregen
  *
  *  This is free software. You can redistribute it and/or modify it under
  *  the terms of Creative Commons Attribution 3.0 United States License. 
@@ -17,12 +18,24 @@
 
 TimerThree Timer3;              // preinstatiate
 
-unsigned int TimerThree::pwmPeriod = 0;
+unsigned short TimerThree::pwmPeriod = 0;
 unsigned char TimerThree::clockSelectBits = 0;
+void (*TimerThree::isrCallback)() = NULL;
 
 // interrupt service routine that wraps a user defined function supplied by attachInterrupt
+#if defined(__AVR__)
 ISR(TIMER3_OVF_vect)
 {
   Timer3.isrCallback();
 }
+
+#elif defined(__arm__) && defined(CORE_TEENSY)
+void ftm2_isr(void)
+{
+  uint32_t sc = FTM2_SC;
+  if (sc & 0x80) FTM2_SC = sc & 0x7F;
+  Timer3.isrCallback();
+}
+
+#endif
 
--- TimerThree.h
+++ TimerThree.h
@@ -4,6 +4,8 @@
  *  Modified March 2009 by Jérôme Despatis and Jesse Tane for ATmega328 support
  *  Modified June 2009 by Michael Polli and Jesse Tane to fix a bug in setPeriod() which caused the timer to stop
  *  Modified April 2012 by Paul Stoffregen - portable to other AVR chips, use inline functions
+ *  Modified again, June 2014 by Paul Stoffregen - support Teensy 3.1 & even more AVR chips
+ *
  *
  *  This is free software. You can redistribute it and/or modify it under
  *  the terms of Creative Commons Attribution 3.0 United States License. 
@@ -34,6 +36,9 @@
 
 class TimerThree
 {
+
+
+#if defined(__AVR__)
   public:
     //****************************
     //  Configuration
@@ -78,15 +83,13 @@ class TimerThree
     void start() __attribute__((always_inline)) {
 	TCCR3B = 0;
 	TCNT3 = 0;		// TODO: does this cause an undesired interrupt?
-	TCCR3B = _BV(WGM33) | clockSelectBits;
+	resume();
     }
     void stop() __attribute__((always_inline)) {
 	TCCR3B = _BV(WGM33);
     }
     void restart() __attribute__((always_inline)) {
-	TCCR3B = 0;
-	TCNT3 = 0;
-	TCCR3B = _BV(WGM33) | clockSelectBits;
+	start();
     }
     void resume() __attribute__((always_inline)) {
 	TCCR3B = _BV(WGM33) | clockSelectBits;
@@ -146,12 +149,142 @@ class TimerThree
     void detachInterrupt() __attribute__((always_inline)) {
 	TIMSK3 = 0;
     }
-    void (*isrCallback)();
+    static void (*isrCallback)();
 
-  protected:
+  private:
     // properties
-    static unsigned int pwmPeriod;
+    static unsigned short pwmPeriod;
     static unsigned char clockSelectBits;
+
+
+
+#elif defined(__arm__) && defined(CORE_TEENSY)
+
+  public:
+    //****************************
+    //  Configuration
+    //****************************
+    void initialize(unsigned long microseconds=1000000) __attribute__((always_inline)) {
+	setPeriod(microseconds);
+    }
+    void setPeriod(unsigned long microseconds) __attribute__((always_inline)) {
+	const unsigned long cycles = (F_BUS / 2000000) * microseconds;
+	if (cycles < TIMER3_RESOLUTION) {
+		clockSelectBits = 0;
+		pwmPeriod = cycles;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 2) {
+		clockSelectBits = 1;
+		pwmPeriod = cycles >> 1;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 4) {
+		clockSelectBits = 2;
+		pwmPeriod = cycles >> 2;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 8) {
+		clockSelectBits = 3;
+		pwmPeriod = cycles >> 3;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 16) {
+		clockSelectBits = 4;
+		pwmPeriod = cycles >> 4;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 32) {
+		clockSelectBits = 5;
+		pwmPeriod = cycles >> 5;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 64) {
+		clockSelectBits = 6;
+		pwmPeriod = cycles >> 6;
+	} else
+	if (cycles < TIMER3_RESOLUTION * 128) {
+		clockSelectBits = 7;
+		pwmPeriod = cycles >> 7;
+	} else {
+		clockSelectBits = 7;
+		pwmPeriod = TIMER3_RESOLUTION - 1;
+	}
+	uint32_t sc = FTM2_SC;
+	FTM2_SC = 0;
+	FTM2_MOD = pwmPeriod;
+	FTM2_SC = FTM_SC_CLKS(1) | FTM_SC_CPWMS | clockSelectBits | (sc & FTM_SC_TOIE);
+    }
+
+    //****************************
+    //  Run Control
+    //****************************
+    void start() __attribute__((always_inline)) {
+	stop();
+	FTM2_CNT = 0;
+	resume();
+    }
+    void stop() __attribute__((always_inline)) {
+	FTM2_SC = FTM2_SC & (FTM_SC_TOIE | FTM_SC_CPWMS | FTM_SC_PS(7));
+    }
+    void restart() __attribute__((always_inline)) {
+	start();
+    }
+    void resume() __attribute__((always_inline)) {
+	FTM2_SC = (FTM1_SC & (FTM_SC_TOIE | FTM_SC_PS(7))) | FTM_SC_CPWMS | FTM_SC_CLKS(1);
+    }
+
+    //****************************
+    //  PWM outputs
+    //****************************
+    void setPwmDuty(char pin, unsigned int duty) __attribute__((always_inline)) {
+	unsigned long dutyCycle = pwmPeriod;
+	dutyCycle *= duty;
+	dutyCycle >>= 10;
+	if (pin == TIMER3_A_PIN) {
+		FTM2_C0V = dutyCycle;
+	} else if (pin == TIMER3_B_PIN) {
+		FTM2_C1V = dutyCycle;
+	}
+    }
+    void pwm(char pin, unsigned int duty) __attribute__((always_inline)) {
+	setPwmDuty(pin, duty);
+	if (pin == TIMER3_A_PIN) {
+		CORE_PIN32_CONFIG = PORT_PCR_MUX(3) | PORT_PCR_DSE | PORT_PCR_SRE;
+	} else if (pin == TIMER3_B_PIN) {
+		CORE_PIN25_CONFIG = PORT_PCR_MUX(3) | PORT_PCR_DSE | PORT_PCR_SRE;
+	}
+    }
+    void pwm(char pin, unsigned int duty, unsigned long microseconds) __attribute__((always_inline)) {
+	if (microseconds > 0) setPeriod(microseconds);
+	pwm(pin, duty);
+    }
+    void disablePwm(char pin) __attribute__((always_inline)) {
+	if (pin == TIMER3_A_PIN) {
+		CORE_PIN32_CONFIG = 0;
+	} else if (pin == TIMER3_B_PIN) {
+		CORE_PIN25_CONFIG = 0;
+	}
+    }
+
+    //****************************
+    //  Interrupt Function
+    //****************************
+    void attachInterrupt(void (*isr)()) __attribute__((always_inline)) {
+	isrCallback = isr;
+	FTM2_SC |= FTM_SC_TOIE;
+	NVIC_ENABLE_IRQ(IRQ_FTM2);
+    }
+    void attachInterrupt(void (*isr)(), unsigned long microseconds) __attribute__((always_inline)) {
+	if(microseconds > 0) setPeriod(microseconds);
+	attachInterrupt(isr);
+    }
+    void detachInterrupt() __attribute__((always_inline)) {
+	FTM2_SC &= ~FTM_SC_TOIE;
+	NVIC_DISABLE_IRQ(IRQ_FTM2);
+    }
+    static void (*isrCallback)();
+
+  private:
+    // properties
+    static unsigned short pwmPeriod;
+    static unsigned char clockSelectBits;
+
+#endif
 };
 
 extern TimerThree Timer3;
